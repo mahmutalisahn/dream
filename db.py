1ab11f21079face8f46ca98cb0b46a77b1fa7b72
sahn111 <you@example.com>
2022-10-23 16:34:52 +0300

    safety

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
+from config_manager import dbname, password, url, user_name
 
-connection_url = f"postgresql://cqbmsisnpozims:9402e3fb09aa82a658d550112857164733910c6cfb328ad428c759a9132fd94e@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com/dbhhbl2m1sggau"
+connection_url = f"postgresql://{user_name}:{password}@{url}/{dbname}"
+print(connection_url)
 sqlalchemy_engine = create_engine(connection_url)
